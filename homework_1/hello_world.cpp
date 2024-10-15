ceec0192a1960d8cb3eea946a1eb993bcd2d2fbe
Матвей Науменко <naumenko33301@gmail.com>
2024-10-16 02:43:36 +0700

    Добавил файлы для выполнения задания, включил move_me_out.cpp, move_me_out.h, и CMakeLists.txt. Обновил .gitignore.

---
+++ homework_1/hello_world.cpp
@@ -0,0 +1,11 @@
+#include "example.h"
+#include "move_me_out.h"
+
+int main() {
+	cout << "Hello, World!" << endl;
+
+	example_function();
+	todo_move_me_out();
+	
+	return 0;
+}
---
+++ homework_1/move_me_out.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+using namespace std;
+
+void todo_move_me_out();
